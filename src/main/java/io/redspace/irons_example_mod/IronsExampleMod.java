ce3b51050a55fe80848b3aa8a89c9c184287beb1
Len Bucchino <len@bitfly.org>
2023-07-03 15:28:31 -0500

    Init

---
+++ src/main/java/io/redspace/irons_example_mod/IronsExampleMod.java
@@ -0,0 +1,49 @@
+package io.redspace.irons_example_mod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(IronsExampleMod.MODID)
+public class IronsExampleMod {
+    public static final String MODID = "irons_example_mod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public IronsExampleMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
---
+++ src/main/java/io/redspace/irons_example_mod/plugin/ExampleSpellPlugin.java
@@ -0,0 +1,26 @@
+package io.redspace.irons_example_mod.plugin;
+
+import io.redspace.irons_example_mod.IronsExampleMod;
+import io.redspace.ironsspellbooks.api.IIronsSpellbooksPlugin;
+import io.redspace.ironsspellbooks.api.IronsSpellbooksPlugin;
+import io.redspace.ironsspellbooks.api.registration.ISpellRegistration;
+import net.minecraft.resources.ResourceLocation;
+
+@IronsSpellbooksPlugin
+public class ExampleSpellPlugin implements IIronsSpellbooksPlugin {
+
+    @Override
+    public ResourceLocation getPluginId() {
+        return new ResourceLocation(IronsExampleMod.MODID, "spell_plugin");
+    }
+
+    @Override
+    public void registerSpells(ISpellRegistration registration) {
+        IronsExampleMod.LOGGER.debug("ExampleSpellPlugin.registerSpells");
+    }
+
+    @Override
+    public void onAllSpellsRegistered() {
+        IronsExampleMod.LOGGER.debug("ExampleSpellPlugin.onAllSpellsRegistered");
+    }
+}
